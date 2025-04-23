f20ce5aa5ec15b0287279cc7d5f08993e32de951
Max Carroll <mjvcaroll@gmail.com>
2025-04-23 16:13:17 +0100

    Cast slicing eval

---
+++ evaluation/CastSliceUtil.ml
@@ -0,0 +1,57 @@
+open Haz3lcore
+
+type slice_info =
+  | ExpCast of Id.t * Exp.t * TypSlice.t * TypSlice.t
+  | ExpCastFail of Id.t * Exp.t * TypSlice.t * TypSlice.t
+  | PatCast of Id.t * Pat.t * TypSlice.t * TypSlice.t
+
+let rec cast_slice_info (e : Exp.t) : slice_info list =
+  match Exp.term_of e with
+  | Cast (e', t1, t2) ->
+      ExpCast (Exp.rep_id e, e', t1, t2) :: cast_slice_info e'
+  | FailedCast (e', t1, t2) ->
+      ExpCastFail (Exp.rep_id e, e', t1, t2) :: cast_slice_info e'
+  | ListLit es | Tuple es | DeferredAp (_, es) ->
+      List.flatten (List.map cast_slice_info es)
+  | Ap (_, e1, e2)
+  | Dot (e1, e2)
+  | TupLabel (e1, e2)
+  | Seq (e1, e2)
+  | Cons (e1, e2)
+  | ListConcat (e1, e2)
+  | BinOp (_, e1, e2)
+  | Let (_, e1, e2) ->
+      cast_slice_info e1 @ cast_slice_info e2
+  | If (e1, e2, e3) ->
+      cast_slice_info e1 @ cast_slice_info e2 @ cast_slice_info e3
+  | TypAp (e1, _) -> cast_slice_info e1
+  | UnOp (_, e)
+  | Test e
+  | Filter (_, e)
+  | Closure (_, e)
+  | Parens e
+  | Probe (e, _) ->
+      cast_slice_info e
+  | Match (e, branches) ->
+      cast_slice_info e
+      @ List.flatten
+          (List.map (fun (p, b) -> pat_casts p @ cast_slice_info b) branches)
+  | Fun (p, e, _, _) | FixF (p, e, _) -> pat_casts p @ cast_slice_info e
+  | TypFun (_, body, _) -> cast_slice_info body
+  | TyAlias (_, _, e) -> cast_slice_info e
+  | Invalid _ | EmptyHole | MultiHole _
+  | DynamicErrorHole (_, _)
+  | Deferral _ | Undefined | Bool _ | Int _ | Float _ | String _ | Constructor _
+  | Label _ | Var _ | BuiltinFun _ ->
+      []
+
+and pat_casts (p : Pat.t) : slice_info list =
+  match Pat.term_of p with
+  | Cast (p', t1, t2) -> PatCast (Pat.rep_id p, p', t1, t2) :: pat_casts p'
+  | ListLit ps | Tuple ps -> List.flatten (List.map pat_casts ps)
+  | Cons (p1, p2) | TupLabel (p1, p2) | Ap (p1, p2) ->
+      pat_casts p1 @ pat_casts p2
+  | Parens p | Probe (p, _) -> pat_casts p
+  | Invalid _ | EmptyHole | MultiHole _ | Wild | Int _ | Float _ | Bool _
+  | String _ | Constructor _ | Label _ | Var _ ->
+      []
--- evaluation/results.ml
+++ evaluation/results.ml
@@ -1,21 +1,47 @@
+open Core_bench
 open Haz3lcore
 open ParseData
 open SlicingUtil
+open CastSliceUtil
 
 let settings = CoreSettings.on (* Note: search off *)
 
+(* Performance Benchmarks *)
+
+(* Basic info relevant to slicing *)
+type expression_info = {
+  term : Exp.t;
+  statics : Statics.Map.t;
+  elaboration : Exp.t;
+  result : DHExp.t; (* Deterministic eval result *)
+}
+
+let make_exp_info e =
+  let statics = Statics.mk settings Builtins.ctx_init e in
+  let elaboration, _ = Elaborator.elaborate statics e in
+  let result, _ = Evaluator.evaluate ~env:Builtins.env_init e in
+  { term = e; statics; elaboration; result }
+
+let ill_typed = ill_typed |> List.map make_exp_info
+let well_typed = well_typed |> List.map make_exp_info
+
+(* Corpus Statistics *)
+
 (* Effectiveness *)
 (* Type Slice Size Stats*)
-let slice_info =
-  ill_typed @ well_typed
-  |> List.map (fun e -> slice_info (Statics.mk settings Builtins.ctx_init e) e)
+let slice_info l =
+  l
+  |> List.map (fun { term; statics; _ } -> slice_info statics term)
   |> List.flatten
 
-(* Total slice size info *)
+let slice_info_all = ill_typed @ well_typed |> slice_info
+
+(* Total slice size info
+   Proportion of size of term + type. Type size approximates the checking context size *)
 type slice_size = { term_type_size : int; slice_size : int; proportion : float }
 
-let slice_sizes_all =
-  slice_info
+let slice_sizes_all l =
+  l
   |> List.map (fun (_, term, slice, _) ->
          (term_size term, term_size (TypSlice slice), slice_size slice))
   |> List.map (fun (term_size, type_size, slice_size) ->
@@ -26,8 +52,8 @@ let slice_sizes_all =
              Float.of_int slice_size /. Float.of_int (term_size + type_size);
          })
 
-let slice_sizes_ok =
-  slice_info
+let slice_sizes_ok l =
+  l
   |> List.filter (function _, _, _, NoTypeError -> true | _ -> false)
   |> List.map (fun (_, term, slice, _) ->
          (term_size term, term_size (TypSlice slice), slice_size slice))
@@ -47,8 +73,8 @@ type slice_size_expectations_error = {
   expectations_slice_size : slice_size;
 }
 
-let slice_sizes_incon_expectations =
-  slice_info
+let slice_sizes_incon_expectations l =
+  l
   |> List.filter_map (function
        | _, term, slice, Inconsistent { syn; ana } ->
            Some
@@ -85,8 +111,8 @@ type slice_size_branches_error = {
   branches_slice_size : slice_size;
 }
 
-let slice_sizes_incon_branches =
-  slice_info
+let slice_sizes_incon_branches l =
+  l
   |> List.filter_map (function
        | _, term, slice, InconsistentBranches ss ->
            Some
@@ -144,4 +170,221 @@ let aggregate_slice_sizes ss =
       w_avg_proportion *. 1.
       /. Float.of_int
            (List.fold_left (fun acc s -> acc + s.term_type_size) 0 ss);
+  }
+
+(* Type Slicing Sizes *)
+let cast_slice_info_elaborated l =
+  l
+  |> List.map (fun { elaboration; _ } -> cast_slice_info elaboration)
+  |> List.flatten
+
+let cast_slice_info_results l =
+  l
+  |> List.map (fun { elaboration; _ } -> cast_slice_info elaboration)
+  |> List.flatten
+
+let cast_slice_info_all =
+  ill_typed @ well_typed |> fun l ->
+  cast_slice_info_elaborated l @ cast_slice_info_results l
+
+(* Slice size info
+   Ratio between slice casted from size and the casted term size
+   Ratio between slice casted to size and it's type *)
+type cast_slice_size = {
+  term_size : int;
+  type_size : int;
+  slice_from_size : int;
+  slice_to_size : int;
+  ratio_from_term : float;
+  ratio_to_type : float;
+}
+
+let cast_slice_sizes_all l =
+  l
+  |> List.map (function
+       | ExpCast (_, e, t1, t2) | ExpCastFail (_, e, t1, t2) ->
+           {
+             term_size = term_size (Exp e);
+             type_size = term_size (TypSlice t2);
+             slice_from_size = slice_size t1;
+             slice_to_size = slice_size t2;
+             ratio_from_term =
+               Float.of_int (slice_size t1) /. Float.of_int (term_size (Exp e));
+             ratio_to_type =
+               Float.of_int (slice_size t2)
+               /. Float.of_int (term_size (TypSlice t2));
+           }
+       | PatCast (_, p, t1, t2) ->
+           {
+             term_size = term_size (Pat p);
+             type_size = term_size (TypSlice t2);
+             slice_from_size = slice_size t1;
+             slice_to_size = slice_size t2;
+             ratio_from_term =
+               Float.of_int (slice_size t1) /. Float.of_int (term_size (Pat p));
+             ratio_to_type =
+               Float.of_int (slice_size t2)
+               /. Float.of_int (term_size (TypSlice t2));
+           })
+
+let cast_slice_sizes_ok l =
+  l
+  |> List.filter_map (function
+       | ExpCastFail _ -> None
+       | ExpCast (_, e, t1, t2) ->
+           Some
+             {
+               term_size = term_size (Exp e);
+               type_size = term_size (TypSlice t2);
+               slice_from_size = slice_size t1;
+               slice_to_size = slice_size t2;
+               ratio_from_term =
+                 Float.of_int (slice_size t1)
+                 /. Float.of_int (term_size (Exp e));
+               ratio_to_type =
+                 Float.of_int (slice_size t2)
+                 /. Float.of_int (term_size (TypSlice t2));
+             }
+       | PatCast (_, p, t1, t2) ->
+           Some
+             {
+               term_size = term_size (Pat p);
+               type_size = term_size (TypSlice t2);
+               slice_from_size = slice_size t1;
+               slice_to_size = slice_size t2;
+               ratio_from_term =
+                 Float.of_int (slice_size t1)
+                 /. Float.of_int (term_size (Pat p));
+               ratio_to_type =
+                 Float.of_int (slice_size t2)
+                 /. Float.of_int (term_size (TypSlice t2));
+             })
+
+let cast_slice_sizes_pats l =
+  l
+  |> List.filter_map (function
+       | ExpCastFail _ | ExpCast _ -> None
+       | PatCast (_, p, t1, t2) ->
+           Some
+             {
+               term_size = term_size (Pat p);
+               type_size = term_size (TypSlice t2);
+               slice_from_size = slice_size t1;
+               slice_to_size = slice_size t2;
+               ratio_from_term =
+                 Float.of_int (slice_size t1)
+                 /. Float.of_int (term_size (Pat p));
+               ratio_to_type =
+                 Float.of_int (slice_size t2)
+                 /. Float.of_int (term_size (TypSlice t2));
+             })
+
+let cast_slice_sizes_errors l =
+  l
+  |> List.filter_map (function
+       | ExpCastFail (_, e, t1, t2) ->
+           Some
+             {
+               term_size = term_size (Exp e);
+               type_size = term_size (TypSlice t2);
+               slice_from_size = slice_size t1;
+               slice_to_size = slice_size t2;
+               ratio_from_term =
+                 Float.of_int (slice_size t1)
+                 /. Float.of_int (term_size (Exp e));
+               ratio_to_type =
+                 Float.of_int (slice_size t2)
+                 /. Float.of_int (term_size (TypSlice t2));
+             }
+       | ExpCast _ | PatCast _ -> None)
+
+type aggregate_cast_slice_size = {
+  avg_term_size : float;
+  avg_type_size : float;
+  avg_slice_from_size : float;
+  avg_slice_to_size : float;
+  w_avg_ratio_from_term : float;
+  w_avg_ratio_to_type : float;
+}
+
+let aggregate_cast_slice_sizes ss =
+  List.fold_left
+    (fun {
+           avg_term_size;
+           avg_type_size;
+           avg_slice_from_size;
+           avg_slice_to_size;
+           w_avg_ratio_from_term;
+           w_avg_ratio_to_type;
+         }
+         {
+           term_size;
+           type_size;
+           slice_from_size;
+           slice_to_size;
+           ratio_from_term;
+           ratio_to_type;
+         } ->
+      {
+        avg_term_size = avg_term_size +. Float.of_int term_size;
+        avg_type_size = avg_type_size +. Float.of_int type_size;
+        avg_slice_from_size =
+          avg_slice_from_size +. Float.of_int slice_from_size;
+        avg_slice_to_size = avg_slice_to_size +. Float.of_int slice_to_size;
+        w_avg_ratio_from_term =
+          w_avg_ratio_from_term +. (Float.of_int term_size *. ratio_from_term);
+        w_avg_ratio_to_type =
+          w_avg_ratio_to_type +. (Float.of_int type_size *. ratio_to_type);
+      })
+    {
+      avg_term_size = 0.;
+      avg_type_size = 0.;
+      avg_slice_from_size = 0.;
+      avg_slice_to_size = 0.;
+      w_avg_ratio_from_term = 0.;
+      w_avg_ratio_to_type = 0.;
+    }
+    ss
+  |>
+  fun {
+        avg_term_size;
+        avg_type_size;
+        avg_slice_from_size;
+        avg_slice_to_size;
+        w_avg_ratio_from_term;
+        w_avg_ratio_to_type;
+      }
+  ->
+  {
+    (* Exclude empty slices from aggregates: these are generally to/fro the dynamic type or unsupported errors/constructs*)
+    avg_term_size =
+      avg_term_size *. 1.
+      /. Float.of_int
+           (ss |> List.filter (fun s -> s.slice_from_size != 0) |> List.length);
+    avg_type_size =
+      avg_type_size *. 1.
+      /. Float.of_int
+           (ss |> List.filter (fun s -> s.slice_to_size != 0) |> List.length);
+    avg_slice_from_size =
+      avg_slice_from_size *. 1.
+      /. Float.of_int
+           (ss |> List.filter (fun s -> s.slice_from_size != 0) |> List.length);
+    avg_slice_to_size =
+      avg_slice_to_size *. 1.
+      /. Float.of_int
+           (ss |> List.filter (fun s -> s.slice_to_size != 0) |> List.length);
+    w_avg_ratio_from_term =
+      w_avg_ratio_from_term *. 1.
+      /. Float.of_int
+           (List.fold_left
+              (fun acc s ->
+                acc + if s.slice_from_size != 0 then s.term_size else 0)
+              0 ss);
+    w_avg_ratio_to_type =
+      w_avg_ratio_to_type *. 1.
+      /. Float.of_int
+           (List.fold_left
+              (fun acc s ->
+                acc + if s.slice_to_size != 0 then s.type_size else 0)
+              0 ss);
   }
